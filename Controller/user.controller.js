7804a538472615fe964a0587189042a27cc36344
operatall <muftauabdulafees18@gmail.com>
2023-11-13 15:40:08 -0800

    made some adjustment with the URI

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -1,64 +1,98 @@
 const jwt = require("jsonwebtoken")
 const userModel = require('../Model/user.model')
+const nodemailer = require("nodemailer");
 
-const Register = (req,res)=>{
+
+const Register = (req, res) => {
    console.log(req.body);
-   let newUser= new userModel(req.body)
+   let newUser = new userModel(req.body)
    newUser.save()
 }
 
-const sigin = (req,res)=>{
-   let {email, password} = req.body
-   userModel.findOne({email:email})
-   .then((user)=>{
-      user.comparedPassword(password,(err, isMatch)=>{
-         let schoolPortal =process.env.SECRET
-         console.log(isMatch);
-         if (isMatch){
-            // res.send({status : true, message: "user found"})
-            jwt.sign({email}, schoolPortal, {expiresIn: '1h'}, (err,token)=>{
+const sigin = (req, res) => {
+   let { email, password } = req.body
+   userModel.findOne({ email: email })
+      .then((user) => {
+         user.comparedPassword(password, (err, isMatch) => {
+            let schoolPortal = process.env.SECRET
+            console.log(isMatch);
+            if (isMatch) {
+               // res.send({status : true, message: "user found"})
+               jwt.sign({ email }, schoolPortal, { expiresIn: '1h' }, (err, token) => {
 
-               if(err){console.log(err);}
-               else{
-                  console.log(token);
-                  res.send({status:true, message:"user found", token:token})
-               }
+                  if (err) { console.log(err); }
+                  else {
+                     console.log(token);
+                     res.send({ status: true, message: "user found", token: token })
+                  }
 
-            })
-            console.log('user found')
-         }
-         else{
-            res.send({status: false, message: "user not found"})
-            console.log('user not found');
-         }
+               })
+               console.log('user found')
+            }
+            else {
+               res.send({ status: false, message: "user not found" })
+               console.log('user not found');
+            }
+         })
+         // console.log("user found");
+
+
       })
-      // console.log("user found");
-      
-        
-   })
-  .catch((err)=>{
-   // console.log(err);
-   console.log("wrong credentials");
-  })
+      .catch((err) => {
+         // console.log(err);
+         console.log("wrong credentials");
+      })
 
 
-  
+
 }
 
-const getDashboard =(req,res)=>{
-   let schoolPortal =process.env.SECRET
+const getDashboard = (req, res) => {
+   let schoolPortal = process.env.SECRET
 
    token = req.headers.authorization.split(" ")[1]
 
-   jwt.verify(token, schoolPortal,(err,result)=>{
-      if(err){
+   jwt.verify(token, schoolPortal, (err, result) => {
+      if (err) {
          console.log(err);
-         res.send({status:false, message:""})
+         res.send({ status: false, message: "" })
       }
-      else{
-         console.log(result);}
-         res.send({status:true, message:"welcome",result})
-   })
+      else {
+         console.log(result);
+      }
+      res.send({ status: true, message: "welcome", result })
 
+   })
 }
-module.exports = {Register, sigin, getDashboard} 
+
+const sendEmail = (req, res)=>{
+   const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+
+        user: "muftauabdulafees18@gmail.com",
+        pass: "dfjqrssmgztenzen",
+      },
+    });
+
+    let mailOptions = {
+      from:"muftauabdulafees18@gmail.com",
+      to: "operatall007@gmail.com",
+      text: "Hello world?", 
+    
+    }
+
+    transporter.sendMail(mailOptions,(error,info)=>{
+      if (error){
+         console.log(error);
+         res.send({status: false, message:"user not created"})
+      }else{
+         console.log("Email sent"+ info.response);
+         res.send({status: true, messages:"user created"})
+      }
+    })
+}
+
+
+
+module.exports = { Register, sigin, getDashboard, sendEmail } 
--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {Register, sigin, getDashboard} = require('../Controller/user.controller')
+const {Register, sigin, getDashboard, sendEmail} = require('../Controller/user.controller')
 
 router.post('/register',Register)
 router.post('/signin', sigin)
 router.get('/dashboard', getDashboard)
+router.get('/sendemail', sendEmail)
 
 module.exports= router 
 
